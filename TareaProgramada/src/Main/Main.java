2c4dbc3090de309c2986d7405787c4db3f7b7e44
Joshua Sancho M <josh.gsm19@gmail.com>
2022-05-29 20:10:30 -0600

    8:10PM

---
+++ TareaProgramada/src/Main/Main.java
@@ -0,0 +1,12 @@
+package Main;
+
+/**
+ * @authors Joshua Sancho M C17337, Luis Hodgson Q C, Marco Zumbado S C
+ * @time 8:07:18 PM
+ * @date May 29, 2022
+ */
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
