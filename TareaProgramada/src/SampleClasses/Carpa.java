6d91f4fb95774cc20a6203dbf6f9e7ee7e160b26
luisedoh1 <luisedoh1@gmail.com>
2022-06-08 19:38:52 -0600

    Imagenes y build

---
+++ TareaProgramada/src/SampleClasses/Carpa.java
@@ -0,0 +1,13 @@
+package SampleClasses;
+
+/**
+ * @author Luis Edo. Hodgson C13822
+ * @time 7:34:09 PM
+ * @date Jun 8, 2022
+ */
+public class Carpa implements Runnable{
+    
+    public void run(){
+    
+    }
+}
