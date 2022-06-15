d7b7827e1578cee3bac009e695fb5aae2b2755aa
luisedoh1 <luisedoh1@gmail.com>
2022-06-14 18:48:56 -0600

    hilos

---
+++ TareaProgramada/src/SampleClasses/Consumer.java
@@ -0,0 +1,45 @@
+package SampleClasses;
+
+/**
+ * @author Luis Edo. Hodgson C13822
+ * @time 8:56:27 PM
+ * @date Jun 13, 2022
+ */
+import java.util.List;
+
+public class Consumer implements Runnable {
+
+   List<String> productos;
+
+   public Consumer(List<String> p) {
+      productos = p;
+   }
+
+   public void run() {
+      synchronized (productos) {
+         
+         if (productos.size() == 0) {
+            try {
+               System.out.println("Hilo: " + Thread.currentThread().getName() +
+                     " Esta en espera wait() ");
+               productos.wait(); 
+               System.out.println("A- Se desperto el hilo: " + Thread.currentThread().getName() +
+                     " Consumio: " + productos.get(productos.size() - 1));
+            } catch (Exception e) {
+               System.out.println("Error: " + e);
+            }
+         }
+         
+         while (productos.size() < 10) {
+            try {
+               productos.wait();
+               System.out.println("B - Se desperto el hilo: " + Thread.currentThread().getName() +
+                     " Consumio: " + productos.get(productos.size() - 1));
+            } catch (Exception e) {
+               System.out.println("Consumer Error: " + e);
+            }
+         }
+
+      }
+   }
+}
---
+++ TareaProgramada/src/SampleClasses/Producer.java
@@ -0,0 +1,34 @@
+package SampleClasses;
+import java.util.List;
+
+/**
+ * @author Luis Edo. Hodgson C13822
+ * @time 9:31:45 PM
+ * @date Jun 13, 2022
+ */
+public class Producer implements Runnable{
+
+   private List<String> productos;
+   
+   
+   public Producer(List<String> p){
+      productos=p;
+   }
+
+   
+   public void run(){
+      System.out.println("Productor se inicia....");
+      for(int i=0;i<10;i++){
+         try{
+            Thread.sleep(500);
+            
+            synchronized (productos){
+               System.out.println("Se agrego producto: "+i);
+               productos.add(String.valueOf(i));
+               
+               productos.notifyAll();
+            }
+         }catch(Exception e){System.out.println("Error: "+e);}
+      }
+   }
+}
